2dabc526d9f847d85c5b29b05149cd00dd8aff84
bob-jr-kab <bobjr520@gmail.com>
2025-06-21 11:45:29 +0300

    Add AntDesign icon and reorganize Settings component layout

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,5 +1,5 @@
 import AppText from "@/components/ui/AppText";
-import { Feather } from "@expo/vector-icons";
+import { AntDesign, Feather } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as Location from "expo-location";
 import { useRouter } from "expo-router";
@@ -114,115 +114,133 @@ export default function Settings() {
       </ImageBackground>
 
       <View className="bg-white -mt-12 rounded-t-3xl border-2 border-gray-200 p-4 shadow-3xl h-5/6">
-        {/* Alerts */}
-        <View className="flex-row justify-between items-center mb-2">
-          <AppText className="text-base">Email Alerts</AppText>
-          <Switch value={emailAlert} onValueChange={setEmailAlert} />
-        </View>
-        <View className="flex-row justify-between items-center mb-4">
-          <AppText className="text-base">Sms Alerts</AppText>
-          <Switch value={smsAlert} onValueChange={setSmsAlert} />
-        </View>
-
-        {/* Emergency Number */}
-        <View className="flex-row justify-between items-center">
-          <AppText className="text-lg font-semibold">Emergency Number</AppText>
-          <TouchableOpacity
-            onPress={() => setIsEditingNumber(!isEditingNumber)}
-          >
-            <Feather name="edit-3" size={16} />
+        <View className="flex-row  gap-2 mb-4">
+          <TouchableOpacity onPress={() => router.push("/home")}>
+            <AntDesign name="arrowleft" size={24} color="black" />
           </TouchableOpacity>
-        </View>
-        {isEditingNumber ? (
-          <TextInput
-            value={emergencyNumber}
-            onChangeText={setEmergencyNumber}
-            className="border rounded-lg px-3 py-2 my-2"
-            keyboardType="phone-pad"
-          />
-        ) : (
-          <AppText className="text-base ml-2 text-gray-500 mb-2">
-            Emergency Number: {emergencyNumber}
+
+          <AppText font="baumans" className="font-bold text-xl">
+            Settings
           </AppText>
-        )}
-
-        {/* Personal Details */}
-        <View className="flex-row justify-between items-center">
-          <AppText className="text-lg font-semibold">Personal Details</AppText>
-          <TouchableOpacity
-            onPress={() => setIsEditingDetails(!isEditingDetails)}
-          >
-            <Feather name="edit-3" size={16} />
-          </TouchableOpacity>
         </View>
 
-        {isEditingDetails ? (
-          <>
-            <TextInput
-              value={name}
-              onChangeText={setName}
-              placeholder="Name"
-              className="border rounded-lg px-3 py-2 my-1"
-            />
-            <TextInput
-              value={apartment}
-              onChangeText={setApartment}
-              placeholder="Apartment"
-              className="border rounded-lg px-3 py-2 my-1"
-            />
-            <TextInput
-              value={address}
-              onChangeText={setAddress}
-              placeholder="Address"
-              className="border rounded-lg px-3 py-2 my-1"
-            />
-          </>
-        ) : (
-          <>
-            <AppText className="text-base ml-2 text-gray-500">
-              Name: {name}
-            </AppText>
-            <AppText className="text-base ml-2 text-gray-500">
-              Apartment: {apartment}
-            </AppText>
-            <AppText className="text-base ml-2 text-gray-500">
-              Address: {address}
-            </AppText>
-          </>
-        )}
+        <View className="ml-4">
+          {/* Alerts */}
+          <View className="flex-row justify-between items-center mb-2">
+            <AppText className="text-base">Email Alerts</AppText>
+            <Switch value={emailAlert} onValueChange={setEmailAlert} />
+          </View>
+          <View className="flex-row justify-between items-center mb-4">
+            <AppText className="text-base">Sms Alerts</AppText>
+            <Switch value={smsAlert} onValueChange={setSmsAlert} />
+          </View>
 
-        {/* Location */}
-        <AppText className="text-base ml-2 mt-4 text-gray-500">
-          Save Current Location:
-        </AppText>
-        {location && (
-          <AppText className="text-sm ml-2 text-gray-500">{location}</AppText>
-        )}
-        <TouchableOpacity
-          onPress={handleGetLocation}
-          className="bg-blue-100 border border-blue-400 rounded-md px-4 py-2 mt-2 mb-4 w-48 items-center"
-        >
-          <AppText className="text-blue-600">Get Location</AppText>
-        </TouchableOpacity>
+          {/* Emergency Number */}
+          <View className="flex-row justify-between items-center">
+            <AppText className="text-lg font-semibold">
+              Emergency Number
+            </AppText>
+            <TouchableOpacity
+              onPress={() => setIsEditingNumber(!isEditingNumber)}
+            >
+              <Feather name="edit-3" size={16} />
+            </TouchableOpacity>
+          </View>
+          {isEditingNumber ? (
+            <TextInput
+              value={emergencyNumber}
+              onChangeText={setEmergencyNumber}
+              className="border rounded-lg px-3 py-2 my-2"
+              keyboardType="phone-pad"
+            />
+          ) : (
+            <AppText className="text-base ml-2 text-gray-500 mb-2">
+              Emergency Number: {emergencyNumber}
+            </AppText>
+          )}
 
-        {/* Save Button */}
-        {(isEditingNumber || isEditingDetails) && (
+          {/* Personal Details */}
+          <View className="flex-row justify-between items-center">
+            <AppText className="text-lg font-semibold">
+              Personal Details
+            </AppText>
+            <TouchableOpacity
+              onPress={() => setIsEditingDetails(!isEditingDetails)}
+            >
+              <Feather name="edit-3" size={16} />
+            </TouchableOpacity>
+          </View>
+
+          {isEditingDetails ? (
+            <>
+              <TextInput
+                value={name}
+                onChangeText={setName}
+                placeholder="Name"
+                className="border rounded-lg px-3 py-2 my-1"
+              />
+              <TextInput
+                value={apartment}
+                onChangeText={setApartment}
+                placeholder="Apartment"
+                className="border rounded-lg px-3 py-2 my-1"
+              />
+              <TextInput
+                value={address}
+                onChangeText={setAddress}
+                placeholder="Address"
+                className="border rounded-lg px-3 py-2 my-1"
+              />
+            </>
+          ) : (
+            <>
+              <AppText className="text-base ml-2 text-gray-500">
+                Name: {name}
+              </AppText>
+              <AppText className="text-base ml-2 text-gray-500">
+                Apartment: {apartment}
+              </AppText>
+              <AppText className="text-base ml-2 text-gray-500">
+                Address: {address}
+              </AppText>
+            </>
+          )}
+
+          {/* Location */}
+          <AppText className="text-base ml-2 mt-4 text-gray-500">
+            Save Current Location:
+          </AppText>
+          {location && (
+            <AppText className="text-sm ml-2 text-gray-500">{location}</AppText>
+          )}
           <TouchableOpacity
-            disabled={!hasChanges}
-            onPress={handleSave}
-            className={`py-3 rounded-xl items-center ${
-              hasChanges ? "bg-blue-500" : "bg-gray-300"
-            }`}
+            onPress={handleGetLocation}
+            className="bg-blue-100 border border-blue-400 rounded-md px-4 py-2 mt-2 mb-4 w-48 items-center"
           >
-            <AppText className="text-white font-semibold">Save</AppText>
+            <AppText className="text-blue-600">Get Location</AppText>
           </TouchableOpacity>
-        )}
 
-        {/* Policies */}
-        <AppText className="text-lg mt-4 font-semibold">Privacy Policy</AppText>
-        <AppText className="text-lg mt-2 font-semibold">
-          Terms and Conditions
-        </AppText>
+          {/* Save Button */}
+          {(isEditingNumber || isEditingDetails) && (
+            <TouchableOpacity
+              disabled={!hasChanges}
+              onPress={handleSave}
+              className={`py-3 rounded-xl items-center ${
+                hasChanges ? "bg-blue-500" : "bg-gray-300"
+              }`}
+            >
+              <AppText className="text-white font-semibold">Save</AppText>
+            </TouchableOpacity>
+          )}
+
+          {/* Policies */}
+          <AppText className="text-lg mt-4 font-semibold">
+            Privacy Policy
+          </AppText>
+          <AppText className="text-lg mt-2 font-semibold">
+            Terms and Conditions
+          </AppText>
+        </View>
 
         {/* Logout */}
         <TouchableOpacity
